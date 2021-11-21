60c95f0560b2489e0fe8bc313e5d8e57cab3c1ca
pritam-dey-19 <dey.pritam19@gmail.com>
2021-11-21 13:38:38 +0530

    Initial commit

---
+++ weather-app/src/components/form.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Form = (props) => {
+  return (
+    <div className="container">
+      <div>{props.error ? error() : null}</div>
+      <form onSubmit={props.loadWeather}>
+        <div>
+          <input
+            className="form-control"
+            type="text"
+            name="city"
+            placeholder="City"
+            autoComplete="off"
+          />
+        </div>
+        <div>
+          <input
+            className="form-control"
+            type="text"
+            name="country"
+            placeholder="Country"
+            autoComplete="off"
+          />
+        </div>
+        <div>
+          <button className="btn btn-secondary">Get weather</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+function error() {
+  return (
+    <div className="alert alert-danger mx-5" role="alert">
+      Please enter City and Country !!
+    </div>
+  );
+}
+
+export default Form;
